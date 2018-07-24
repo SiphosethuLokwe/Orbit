c00bb5964b990f70f1bc3b927596186ebe387d56
SiphosethuLokwe <sethucarter@gmail.com>
2018-07-24 14:18:52 +0200

    isa project

---
+++ .history/resources/views/navbar.blade_20180724122651.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-default header-navigation stricky">
+    <div class="container clearfix">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".main-navigation" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index-2.html">
+                <img src="img/logo.png" alt="Awesome Image" class="default-logo" />
+                <img src="img/logo-2.png" alt="Awesome Image" class="stick-logo" />
+            </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse main-navigation mainmenu one-page-scroll-menu" id="main-nav-bar">
+            
+            <ul class="nav navbar-nav navigation-box">
+                <li class="scrollToLink current"> <a href="#banner">Home</a> </li>
+                <li class="scrollToLink"> <a href="#features">Features</a> </li>
+                <li class="scrollToLink"> <a href="#how-it-works">How It Works</a> </li>
+                <li class="scrollToLink"> <a href="#pricing">Pricing</a> </li>
+                <li class="scrollToLink"> 
+                    <a href="#blog">Blog</a> 
+                    <ul class="sub-menu">
+                        <li><a href="blog.html">Blog Listing</a></li>
+                        <li><a href="blog-details.html">Blog Details</a></li>
+                    </ul><!-- /.sub-menu -->
+                   </li>
+                <li class="show-mobile"> <a href="#">sign in</a> </li>
+            </ul>
+            
+        </div><!-- /.navbar-collapse -->
+        <div class="right-side-box">
+            <a href="#" class="sign-btn">sign in</a>
+        </div><!-- /.right-side-box -->
+    </div><!-- /.container -->
+</nav>   
